7954a619a87f2e0a77ea02193c117ea43f013f5b
Denis Rodionov <nicholas.hawkwood@gmail.com>
2021-09-05 14:34:43 +0300

    Комментарии

--- client/src/components/ColoredValue.vue
+++ client/src/components/ColoredValue.vue
@@ -1,3 +1,5 @@
+<!-- Отображает зеленым или красным динамику изменения значения на вход, например красное –10 или зеленое +100 -->
+
 <template>
   <span class="value" :class="colorClass">{{ sign }}{{ displayValue }}</span>
 </template>
@@ -6,6 +8,7 @@
 import { computed, defineProps } from 'vue'
 
 const props = defineProps({
+  /** Значение на входе */
   value: {
     type: Number,
     required: true,
@@ -13,30 +16,36 @@ const props = defineProps({
   },
 })
 
+/** Знак плюс или минус перед числом */
 const sign = computed(() => {
   if (props.value > 0) {
     return '+'
   } else if (props.value < 0) {
+    // Минус - среднего размера, не дефис, что правильнее с точки зрения типографики
     return '–'
   } else {
     return ''
   }
 })
 
+/** Цвет числа */
 const colorClass = computed(() => {
   if (props.value > 0) {
     return 'green'
   } else if (props.value < 0) {
     return 'red'
   } else {
+    // Для нуля цвет не меняется
     return ''
   }
 })
 
+/** Отображаеме значение числа */
 const displayValue = computed(() => {
   if (props.value >= 0) {
     return props.value
   } else {
+    // Отрицательное число отображаем по модулю, потому что у нас сделана отдельная логика для более типографически правильного минуса
     return -1 * props.value
   }
 })
